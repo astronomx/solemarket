6c79cedc4cbd3919c442130a0e0ecd560904bda0
astronomx <quentinvalen@gmail.com>
2023-10-05 09:54:15 +0200

    Navbar, Homepage and catalog working

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -4,7 +4,7 @@ import FilterMenu from "@/components/FilterMenu";
 export default function Catalog() {
   return(
     <>
-      <div className="flex justify-center">
+      <div className="flex justify-center mt-10">
         <div className="flex flex-row">
           <FilterMenu />
           <ShoeCatalog />
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,119 @@
+"use client";
+
+import Link from 'next/link';
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon } from "@heroicons/react/24/outline";
+import { ShoppingCartIcon } from "@heroicons/react/24/outline";
+import React, { useState } from "react";
+
+export default function Navbar() {
+  const [showOffcanvas, setShowOffcanvas] = useState(false);
+
+  const handleToggleOffcanvas = () => {
+    setShowOffcanvas(!showOffcanvas);
+  };
+
+  return (
+    <nav className="flex justify-between items-center border-b border-black bg-white p-4 top-0 sticky">
+      <div className="flex items-center">
+        <button onClick={handleToggleOffcanvas}>
+          <Bars3Icon className="h-6 w-6 text-gray-500" />
+        </button>
+        <div className="w-6 h-6 bg-white"></div>
+        <div className="relative">
+          <input
+            type="text"
+            placeholder="Search"
+            className="border rounded-full py-1 px-3 pr-10 focus:outline-none focus:border-green-700"
+          />
+          <div className="absolute top-0 right-0 mt-2 mr-3">
+            <button><MagnifyingGlassIcon className="h-5 w-5 text-gray-800" /></button>
+          </div>
+        </div>
+      </div>
+      <div className="hidden md:flex space-x-20">
+        <Link href="/" passHref>
+          <span className="font-bold text-lg hover:text-[#098C4C]">Home</span>
+        </Link>
+        <Link href="/" passHref>
+          <span className="font-bold text-lg hover:text-[#098C4C]">Brands</span>
+        </Link>
+        <Link href="/" passHref>
+          <span className="font-bold text-lg hover:text-[#098C4C]">New Arrivals</span>
+        </Link>
+        <Link href="/catalog" passHref>
+          <span className="font-bold text-lg hover:text-[#098C4C] focus:text-[#098C4C]">All</span>
+        </Link>
+        <Link href="/" passHref>
+          <span className="font-bold text-lg hover:text-[#098C4C]">Trending</span>
+        </Link>
+      </div>
+
+      <div className="hidden md:flex items-center space-x-4">
+        <button className="font-bold text-[#098C4C] py-2 px-4">Login</button>
+        <span className="font-bold text-gray-800">|</span>
+        <button className="font-bold text-black py-2 px-4">Register</button>
+        <button><ShoppingCartIcon className="h-6 w-6 text-black" /></button>
+      </div>
+
+      {showOffcanvas && (
+        <div className="fixed inset-0 bg-gray-700 bg-opacity-50 z-50">
+          <div className="absolute inset-y-0 left-0 max-w-xs w-full bg-[#2C2C2C] shadow-xl bg-[#2C2C2C]">
+            <div className="flex justify-end">
+              <button onClick={handleToggleOffcanvas} className="p-4">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
+                </svg>
+              </button>
+            </div>
+            <div className="py-4 px-8">
+              <p className="mt-4">
+                <Link href="/" passHref>
+                  <span className="font-bold text-lg text-white hover:text-[#098C4C]">Home</span>
+                </Link>
+                <br />
+                <Link href="/" passHref>
+                  <span className="font-bold text-lg text-white hover:text-[#098C4C]">Brands</span>
+                </Link>
+                <br />
+                <Link href="/catalog" passHref>
+                  <span className="font-bold text-lg text-white hover:text-[#098C4C]">All</span>
+                </Link>
+                <br />
+                <Link href="/" passHref>
+                  <span className="font-bold text-lg text-white hover:text-[#098C4C]">Trending</span>
+                </Link>
+                <br />
+                <Link href="/" passHref>
+                  <span className="font-bold text-lg text-white hover:text-[#098C4C]">New Arrival</span>
+                </Link>
+                <br />
+                <Link href="/" passHref>
+                  <span className="font-bold text-lg text-white hover:text-[#098C4C]">Coming soon</span>
+                </Link>
+                <br />
+                <Link href="/Selling" passHref>
+                  <span className="font-bold text-lg text-white hover:text-[#098C4C]">Selling</span>
+                </Link>
+                <br />
+                <Link href="/Buying" passHref>
+                  <span className="font-bold text-lg text-white hover:text-[#098C4C]">Buying</span>
+                </Link>
+              </p>
+            </div>
+
+            <div className="absolute bottom-0 left-0 w-full py-4 px-8 space-x-2">
+              <Link href="/Login" passHref>
+                <span className="font-bold text-lg text-white hover:text-[#098C4C]">Login</span>
+              </Link>
+              <span className="font-bold text-white">|</span>
+              <Link href="/Register" passHref>
+                <span className="font-bold text-lg text-white hover:text-[#098C4C]">Register</span>
+              </Link>
+            </div>
+          </div>
+        </div>
+      )}
+    </nav>
+  );
+}
