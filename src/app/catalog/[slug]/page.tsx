e2e58bb969c9eb95ac9e3c175db7f4b05da1f6cd
astronomx <quentinvalen@gmail.com>
2023-10-09 22:30:27 +0200

    Correct data is displayed on screen when a shoe is pressed

--- src/app/catalog/[slug]/page.tsx
+++ src/app/catalog/[slug]/page.tsx
@@ -52,28 +52,64 @@
 // }
 'use client'
 
-import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useParams, useRouter } from "next/navigation";
+import Image from "next/image";
+
+// dit is een supabase client die we hebben aangemaakt in de config folder. zodat we de supabase functies kunnen gebruiken.
+import supabase from "@/config/supabaseClient";
 
 import { ArrowPathIcon } from "@heroicons/react/24/outline";
 
 
-// export default async function ShoeDetails({ shoe }: { shoe: { id: number; name: string; price: number; imageURL: any, slug: string } }) {
 export default function ShoeDetails() {
 
-    const { slug } = useParams();
     
-    let shoe: { id: any, name: string, price: number, imageURL: any, slug: string }[] = [];
+    const { slug } = useParams();
+    const router = useRouter();
+    
+    const [shoeName, setShoeName] = useState<string>("");
+    const [shoePrice, setShoePrice] = useState<number>(0);
+    const [shoeImageURL, setShoeImageURL] = useState<any>("");
+    const [shoeSlug, setShoeSlug] = useState<string>("");
 
+    const shoe = [shoeName, shoePrice, shoeImageURL, shoeSlug];
+    
+    useEffect(() => {
+        const getShoeDetails = async () => {
+                // Hier vergelijkem we de slug die we hebben meegegeven in de URL met de slug die in de database staat.
+                const { data, error } = await supabase
+                .from("shoes")
+                .select("id, name, price, imageURL, slug")
+                .eq("slug", slug)
+                .single();
+
+                // Geeft een error als er iets mis is gegaan met het ophalen van de data. En anders returnt het de data.
+                if(error) {
+                    router.push("/404");
+                }
+            
+                // Deze data wordt gapkt vanuit de database door de slug te vergelijken met de slug die we hebben meegegeven in de URL.
+                if(data) {
+                  setShoeName(data.name);
+                  setShoePrice(data.price);
+                  setShoeImageURL(data.imageURL);
+                  setShoeImageURL(data.slug);
+                }
+            }
+            
+            getShoeDetails();
+            
+    }, [slug, router])
 
 
     return (
         <div className="flex justify-center items-center h-screen">
-            {shoe ? (
+            {shoe  ? (
                 <>
-                    {/* <h1 className="text-bold">{shoe.name}</h1>
-                    <p>ID: {shoe.id}</p>
-                    <p>Price: {shoe.price}</p> */}
-                    <h1>Param: {slug}</h1>
+                    <h1 className="font-bold">{shoe[0]}</h1>
+                    <p className="text-[#098C4C]">{shoe[1]}</p>
+                    
                 </>
             ) : (
                 <div className="flex justify-center items-center">
