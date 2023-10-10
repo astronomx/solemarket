bcc08220e9b0e3866c48221993b75f383a5dfa6c
astronomx <quentinvalen@gmail.com>
2023-10-10 07:54:07 +0200

    Featured page also has product page function

--- src/app/catalog/[slug]/page.tsx
+++ src/app/catalog/[slug]/page.tsx
@@ -13,25 +13,30 @@ export default function ShoeDetails() {
     const { slug } = useParams();
     const router = useRouter();
 
-    // Define a default value for shoeData, which will be an empty object
+    // Hier maken we een state aan voor de shoeData. Deze state is een object met de volgende properties: name, price, imageURL, slug. De type is <any>
+    // omdat we nog niet weten wat voor data we terug krijgen van de supabase query.
     const [shoeData, setShoeData] = useState<any>({});
 
+    // Met de useEffect hook gaan we de data ophalen van de shoe die we willen laten zien. Dit doen we door de slug te gebruiken die we hebben meegegeven in de url.
     useEffect(() => {
         const getShoeDetails = async () => {
+            // Met de .eq() functie kunnen we een waarde meegeven die we willen gebruiken om te filteren. In dit geval gebruiken we de slug om de juiste shoe op te halen.
+            // De .single() functie zorgt ervoor dat we maar 1 resultaat terug krijgen. Als we dit niet doen krijgen we een array terug met 1 object.
             try {
                 const { data, error } = await supabase
                     .from("shoes")
                     .select("id, name, price, imageURL, slug")
                     .eq("slug", slug)
                     .single();
-
+                
+                // Als er een error is dan sturen we de gebruiker naar de 404 pagina.
                 if (error) {
                     router.push("/404");
                     return;
                 }
 
                 if (data) {
-                    // Destructure and set the shoeData state
+                    // Als er geen error is dan zetten we de data in de shoeData state.
                     const { name, price, imageURL, slug } = data;
                     setShoeData({ name, price, imageURL, slug });
                 }
@@ -41,10 +46,12 @@ export default function ShoeDetails() {
         };
 
         getShoeDetails();
+        // We voegen de slug en router toe aan de dependency array zodat de useEffect hook opnieuw wordt uitgevoerd als de slug of router veranderd.
     }, [slug, router]);
 
     return (
         <div className="flex justify-center items-center h-screen">
+            {/* Hier renderen we de data van de shoeData state. Als de shoeData state leeg is dan laten we een loading icoon zien. */}
             {shoeData.name ? (
                 <>
                     <h1 className="font-bold">{shoeData.name}</h1>
--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,15 +1,12 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
+import Link from "next/link";
+import supabase from "@/config/supabaseClient";
 
 async function getShoes() {
-    const supabase = createServerComponentClient({
-      cookies,
-    });
   
     // fetching data vanuit supabase, we gebruiken de range() methode om de eerste 15 rows op te halen en displayen in de featured sectie.
     const { data, error } = await supabase
       .from("shoes")
-      .select("id, name, price, imageURL")
+      .select("id, name, price, imageURL, slug")
       .range(0, 5);
   
     // Geeft een error als er iets mis is gegaan met het ophalen van de data. En anders returnt het de data.
@@ -22,7 +19,7 @@ async function getShoes() {
 
 export default async function Featured() {
 
-    let shoes: { id: number, name: string, price: number, imageURL: any }[] = [];
+    let shoes: { id: number, name: string, price: number, imageURL: any, slug: string }[] = [];
 
     try {
         shoes = await getShoes();
@@ -37,7 +34,7 @@ export default async function Featured() {
                 <h1 className="text-2xl text-[#098C4C] font-bold mb-4 ml-4">Featured</h1>
                 <div className="flex justify-center space-x-5">
                 {shoes.map((shoe) => (
-                    <div key={shoe.id}>
+                    <Link href={`/catalog/${shoe.slug}`} key={shoe.id}>
                         <div className="relative flex-col justify-center border-4 xl:border-2 p-4 rounded-md w-[180px] md:w-[230px] h-[250px] ease-in-out duration-300 hover:border-[#098C4C] hover:w-[240px] hover:h-[260px]">
                           <div className="flex justify-center">
                             <img src={shoe.imageURL} className="w-32 self-center h-auto" alt="picture" />
@@ -49,7 +46,7 @@ export default async function Featured() {
                             <p className="text-base md:text-xl text-[#098C4C]">€{shoe.price}</p>
                             </div>
                         </div>
-                    </div>
+                    </Link>
                 ))}
                 </div>
             </div>
