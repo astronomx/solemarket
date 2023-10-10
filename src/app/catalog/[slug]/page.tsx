da2a24a0fcc546a2ce3b94672e1c1e9965c7f525
astronomx <quentinvalen@gmail.com>
2023-10-10 07:42:32 +0200

    Everything is loading the name price and image are correctly displaying on the page

--- src/app/catalog/[slug]/page.tsx
+++ src/app/catalog/[slug]/page.tsx
@@ -9,66 +9,6 @@ import supabase from "@/config/supabaseClient";
 
 import { ArrowPathIcon } from "@heroicons/react/24/outline";
 
-
-// export default function ShoeDetails() {
-
-    
-//     const { slug } = useParams();
-//     const router = useRouter();
-    
-//     const [shoeName, setShoeName] = useState<string>("");
-//     const [shoePrice, setShoePrice] = useState<number>(0);
-//     const [shoeImageURL, setShoeImageURL] = useState<any>("");
-//     const [shoeSlug, setShoeSlug] = useState<string>("");
-
-//     const shoe = [shoeName, shoePrice, shoeImageURL, shoeSlug];
-    
-//     useEffect(() => {
-//         const getShoeDetails = async () => {
-//                 // Hier vergelijkem we de slug die we hebben meegegeven in de URL met de slug die in de database staat.
-//                 const { data, error } = await supabase
-//                 .from("shoes")
-//                 .select("id, name, price, imageURL, slug")
-//                 .eq("slug", slug)
-//                 .single();
-
-//                 // Geeft een error als er iets mis is gegaan met het ophalen van de data. En anders returnt het de data.
-//                 if(error) {
-//                     router.push("/404");
-//                 }
-            
-//                 // Deze data wordt gapkt vanuit de database door de slug te vergelijken met de slug die we hebben meegegeven in de URL.
-//                 if(data) {
-//                   setShoeName(data.name);
-//                   setShoePrice(data.price);
-//                   setShoeImageURL(data.imageURL);
-//                   setShoeImageURL(data.slug);
-//                 }
-//             }
-            
-//             getShoeDetails();
-            
-//     }, [slug, router])
-
-
-//     return (
-//         <div className="flex justify-center items-center h-screen">
-//             {shoe  ? (
-//                 <>
-//                     <h1 className="font-bold">{shoe[0]}</h1>
-//                     <p className="text-[#098C4C]">{shoe[1]}</p>
-                    
-//                 </>
-//             ) : (
-//                 <div className="flex justify-center items-center">
-//                     <div className="text-3xl text-bold">Loading</div>
-//                     <ArrowPathIcon className="h-6 w-6 text-balck animate-spin" />
-//                 </div>
-//             )}
-//         </div>
-//     );
-// }
-
 export default function ShoeDetails() {
     const { slug } = useParams();
     const router = useRouter();
@@ -114,7 +54,7 @@ export default function ShoeDetails() {
             ) : (
                 <div className="flex justify-center items-center">
                     <div className="text-3xl text-bold">Loading</div>
-                    {/* ... (loading icon or message) */}
+                    <ArrowPathIcon className="animate-spin h-9 w-9" />
                 </div>
             )}
         </div>
