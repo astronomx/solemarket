e6062fc49060145219f46272039393f11372829f
astronomx <quentinvalen@gmail.com>
2023-10-16 21:47:40 +0200

    Made the carousel change on it's own

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { ArrowLeftCircleIcon, ArrowRightCircleIcon } from "@heroicons/react/24/outline";
 
@@ -23,13 +23,30 @@ export default function Hero() {
     setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
   };
 
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 5000);
+
+    return () => clearTimeout(timeout);
+  }, [currentIndex]);
+
   return (
     <>
       <div className="relative">
         <div className="flex flex-row snap-x snap-mandatory overflow-scroll w-full h-[50vh]">
           {images.map((image, index) => (
-            <div key={index} className={`flex items-center justify-center flex-shrink-0 snap-center w-screen transition-opacity duration-500 ${index === currentIndex ? 'opacity-100' : 'opacity-0 hidden'}`}>
-              <img src={image} alt={`carousel-item-${index}`} className="rounded-lg drop-shadow-xl 2xl:w-[40vw] h-[45vh] sm:w-[350px] h-[200px] md:w-[70vw] h-[40vh] lg:w-[50vw] h-[30vh] xl:w-[50vw] h-[30vh]" />
+            <div
+              key={index}
+              className={`flex items-center justify-center flex-shrink-0 snap-center w-screen transition-opacity duration-500 ${
+                index === currentIndex ? 'opacity-100' : 'opacity-0 hidden'
+              }`}
+            >
+              <img
+                src={image}
+                alt={`carousel-item-${index}`}
+                className="rounded-lg drop-shadow-xl 2xl:w-[40vw] h-[45vh] sm:w-[350px] h-[200px] md:w-[70vw] h-[40vh] lg:w-[50vw] h-[30vh] xl:w-[50vw] h-[30vh]"
+              />
             </div>
           ))}
         </div>
