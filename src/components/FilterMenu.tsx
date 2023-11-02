0a3f52ec35f8d13643f0755ab41649a08d59072a
astronomx <quentinvalen@gmail.com>
2023-11-02 13:54:12 +0100

    Added styling for the shoe cards

--- src/components/FilterMenu.tsx
+++ src/components/FilterMenu.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
+import Link from "next/link";
 import supabase from "@/config/supabaseClient";
 
 export default function FilterMenu() {
@@ -69,13 +70,33 @@ export default function FilterMenu() {
       </div>
 
       <div>
-        {products.map((product: any) => (
+        {/* {products.map((product: any) => (
           <div key={product.id}>
             <h2>{product.name}</h2>
             <p>{product.price}</p>
             <img src={product.imageURL} alt={product.name} />
           </div>
-        ))}
+        ))} */}
+        <div className="flex flex-wrap justify-center gap-3 md:w-[900px]">
+          {products.map((shoe) => (
+            <div key={shoe.id} className="relative">
+              <Link href={`/catalog/${shoe.slug}`}>
+                <div className="flex flex-col justify-center border-4 xl:border-2 p-4 rounded-md w-[180px] md:w-[230px] h-[250px] ease-in-out duration-300 hover:border-[#098C4C] hover:w-[240px] hover:h-[260px]">
+                  <img src={shoe.imageURL} className="w-32 self-center h-auto" width={128} height={128} alt="picture" loading="lazy"
+                  />
+                  <div className="flex flex-col mt-2">
+                    <p className="font-bold text-sm md:text-lg">{shoe.name}</p>
+                  </div>
+                  <div className="absolute top-4 left-4 z-0">
+                    <p className="text-base md:text-xl text-[#098C4C]">
+                      €{shoe.price}
+                    </p>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
